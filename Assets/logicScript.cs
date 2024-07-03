89f789469762033c1cf28a7d5ccf9aa96d569751
Peter Wu <petercwu1205@gmail.com>
2024-07-02 22:17:31 -0700

    added logic manager and buttons to reset scene

---
+++ Assets/logicScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class logicScript : MonoBehaviour
+{
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
