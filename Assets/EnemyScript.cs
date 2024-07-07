96738e4510a8f74cf7dd7af9bdb83419da04f137
Peter Wu <petercwu1205@gmail.com>
2024-07-07 00:29:00 -0700

    added level three enemy counter, error in arrow spawning

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public static int enemyCount = 0;
+    public GameObject arrowSprite;
+    public GameObject bowSprite;
+    private Vector3 arrowStartingPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        arrowStartingPosition = arrowSprite.transform.position;
     }
 
     // Update is called once per frame
@@ -16,12 +21,24 @@ public class EnemyScript : MonoBehaviour
         
     }
 
+    public void Awake()
+    {
+        enemyCount++;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Arrow")
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
+            Debug.Log("Enemies Count:" + enemyCount);
+            enemyCount--;
+
+            if (enemyCount > 0) {
+                Debug.Log("Arrow Sprite Starting Position2: " + arrowStartingPosition);
+                Instantiate(arrowSprite, arrowStartingPosition, bowSprite.transform.rotation);
+            }
         }   
     }
 }
